0808947431b57278961458542d8ed08757df5797
Azhgaliev <Sport-titan@mail.ru>
2020-07-01 17:48:21 +0600

    initial

---
+++ qzhub_roles/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "qzhub_roles",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        create own big group with accesses
+    """,
+
+    'author': "QZHub",
+    'website': "ppm.qzhub.com",
+
+    'category': 'Security',
+    'version': '0.1',
+
+
+    'depends': ['base',
+                'base_user_role'],
+
+
+    'data': [
+        'views/qzh_role_template.xml',
+    ],
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
---
+++ qzhub_roles/models/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+from . import qzh_roles
